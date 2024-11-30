29eada17c046371e869ad1e02b9af7178218a819
ChoiKangSub <cks4718@gmail.com>
2024-11-30 17:51:22 +0900

    레이어드 아키텍처 연습

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -9,9 +9,11 @@ import sample.cafekiosk.spring.domain.order.OrderRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
 import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -38,8 +40,24 @@ public class OrderService {
                 .collect(Collectors.toList());
 
         //재고 엔티티 조회
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        Map<Long, Stock> stockMap = stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
         //상품별 counting
+        Map<Long, Long> productCountingMap = stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+
         //재고 차감 시도
+        for (Long stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+            if(stock.isQuantityLessThan(quantity)){
+
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+
+        }
 
 
         Order order =  Order.create(duplicateProducts,registeredDateTime);
--- src/test/java/sample/cafekiosk/spring/domain/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderServiceTest.java
@@ -23,6 +23,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 
 @ActiveProfiles("test")
@@ -86,8 +87,10 @@ public class OrderServiceTest {
 
     @Test
     @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
-    public void createOrderWithStock(){
+     public void createOrderWithStock(){
         // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
         Product product = createProduct(ProductType.BOTTLE,001L,1000);
         Product product2 = createProduct(ProductType.BAKERY,002L,3000);
         Product product3 = createProduct(ProductType.HANDMADE,003L,5000);
@@ -96,14 +99,14 @@ public class OrderServiceTest {
 
 
         Stock stock1 = Stock.create(001L,2);
-        Stock stock2 = Stock.create(001L,2);
+        Stock stock2 = Stock.create(002L,2);
         stockRepository.saveAll(List.of(stock1, stock2));
 
         // when
         OrderCreateRequest request = OrderCreateRequest.builder()
                 .productNumbers(List.of(001L, 001L, 002L, 003L))
                 .build();
-        LocalDateTime registeredDateTime = LocalDateTime.now();
+
         OrderResponse orderResponse = orderService.createOrder(request,registeredDateTime);
 
         // then
@@ -127,6 +130,39 @@ public class OrderServiceTest {
                         tuple(001L,0),
                         tuple(002L,1)
                 );
+    }
+
+    @Test
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+    public void createOrderWithNoStock(){
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product = createProduct(ProductType.BOTTLE,001L,1000);
+        Product product2 = createProduct(ProductType.BAKERY,002L,3000);
+        Product product3 = createProduct(ProductType.HANDMADE,003L,5000);
+
+        productRepository.saveAll(List.of(product, product2, product3));
+
+
+        Stock stock1 = Stock.create(001L,2);
+        Stock stock2 = Stock.create(002L,2);
+        stock1.deductQuantity(1);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of(001L, 001L, 002L, 003L))
+                .build();
+
+        // when//then
+
+
+
+        assertThatThrownBy(()->orderService.createOrder(request,registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("재고가 부족한 상품이 있습니다.");
+
+
     }
 
     @Test
