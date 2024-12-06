8431aa643037674ee856c9d75eb852691aa75189
ChoiKangSub <cks4718@gmail.com>
2024-12-06 15:28:08 +0900

    guava

---
+++ src/test/java/sample/cafekiosk/learning/GuavaLearningTest.java
@@ -0,0 +1,27 @@
+package sample.cafekiosk.learning;
+
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GuavaLearningTest {
+
+
+    @Test
+    @DisplayName("주어진 개수만큼 리스트를 파티셔닝한다")
+    public void partitionLearningTest (){
+        // given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+        // when
+        List<List<Integer>> partition = Lists.partition(integers, 3);
+        // then
+        assertThat(partition).hasSize(2)
+                .isEqualTo(List.of(
+                        List.of(1,2,3),List.of(4,5,6)
+                ));
+    }
+}
